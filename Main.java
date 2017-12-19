a8011ba376134c9d22c20871db401f80df3e8210
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-19 11:08:18 +0100

    Making order in main class

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package pl.fyv;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.print("Podaj zakres petli: "); int n = s.nextInt();
+        System.out.print("Podaj zakres petli: ");
+        Engine e = new Engine(s.nextInt());
 
-        ArrayList<String> list = new ArrayList<String>();
-
-
-
-        for(int i=1;i<n;i++) {
-            list.add(String.valueOf(i));
-            if(i % 3 == 0) { list.remove(i); list.add(i, "Fizz"); }
-            if(i % 5 == 0) { list.remove(i); list.add(i, "Buzz"); }
-            if(i % 3 == 0 && i % 5 == 0) { list.remove(i); list.add(i, "FizzBuzz"); }
-        }
-
-            System.out.println(list);
-
+        e.FizzBuzzGame();
     }
 }
